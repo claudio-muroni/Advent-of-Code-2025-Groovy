e20df69c1cc46e056bfea675210e9d085222454e
claudio-muroni <muroni.claudio@gmail.com>
2026-03-01 19:15:38 +0100

    Solved Day 01 part 2

--- src/Day01_2.groovy
+++ src/Day01_2.groovy
@@ -9,7 +9,6 @@ static void main(String[] args) {
         def clickNumber = line.substring(1).toInteger()
 
         Integer round = clickNumber / 100
-        //println("round: "+round)
         clickNumber = clickNumber % 100
 
         def newPos = direction=="R" ? pos + clickNumber : pos - clickNumber
@@ -17,13 +16,9 @@ static void main(String[] args) {
         if (newPos > 99) newPos -= 100
         else if (newPos < 0) newPos += 100
 
-        //println(newPos)
-
         pw += round
         if (newPos == 0) pw++
         else if (pos!=0 && ((newPos > pos && direction == "L") || (newPos < pos && direction == "R"))) pw++
-
-        //println("pw: " + pw)
 
         pos = newPos
     }
