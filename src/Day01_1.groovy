0ad3e616ed138c879aa450afee139142c6972fb9
claudio-muroni <muroni.claudio@gmail.com>
2026-03-01 18:37:27 +0100

    Solved Day 01 part 1

--- src/Day01_1.groovy
+++ src/Day01_1.groovy
@@ -1,4 +1,24 @@
 static void main(String[] args) {
     def lines = new File("resources/Input_Day01_1.txt").readLines()
-    println(lines[0])
+
+    def pw = 0
+    def pos = 50
+    for (line in lines) {
+        def direction = line.substring(0,1)
+
+        def clickNumber = line.substring(1)
+        if (clickNumber.length() > 2) clickNumber = clickNumber.substring(clickNumber.length()-2)
+        clickNumber = clickNumber.toInteger()
+
+        pos = direction=="R" ? pos + clickNumber : pos - clickNumber
+
+        if (pos > 99) pos -= 100
+        else if (pos < 0) pos += 100
+
+        //println(pos)
+
+        if (pos == 0) pw++
+    }
+
+    println("Day 01 part 1 solution: " + pw)
 }
