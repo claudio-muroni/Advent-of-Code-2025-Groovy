ce9352194c70f3604c891460297e8e1a6d40ce7e
claudio-muroni <muroni.claudio@gmail.com>
2026-03-05 21:28:42 +0100

    Solved Day 02 part 1

--- src/Day02_1.groovy
+++ src/Day02_1.groovy
@@ -1,5 +1,22 @@
 static void main() {
     def line = new File("resources/Input_Day02_1.txt").readLines()[0]
+    def rangesList = line.split(',')
 
+    BigInteger invId = 0
 
+    for (range in rangesList) {
+        def start = range.split('-')[0].toBigInteger()
+        def end = range.split('-')[1].toBigInteger()
+
+        for (num in start..end) {
+            def numStr = num.toString()
+            if (numStr.length()%2==0) {
+                if ( numStr.substring(0, (numStr.length()/2).toInteger()) == numStr.substring((numStr.length()/2).toInteger()) ) {
+                    invId+=numStr.toBigInteger()
+                }
+            }
+        }
+    }
+
+    println("Day 02 part 1 solution: "+invId)
 }
