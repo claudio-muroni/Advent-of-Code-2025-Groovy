fdb45a1b773089bf189736e59175c122fab291b4
claudio-muroni <muroni.claudio@gmail.com>
2026-03-06 23:40:44 +0100

    Solved Day 02 part 2

---
+++ src/Day02_2.groovy
@@ -0,0 +1,23 @@
+static void main() {
+    def line = new File("resources/Input_Day02_1.txt").readLines()[0]
+    def rangesList = line.split(',')
+
+    BigInteger invId = 0
+
+    for (range in rangesList) {
+        def start = range.split('-')[0].toBigInteger()
+        def end = range.split('-')[1].toBigInteger()
+
+        for (num in start..end) {
+            def numStr = num.toString()
+            def isRepeated = numStr ==~ /^(.+)\1+$/
+            if (isRepeated) {
+                invId+=numStr.toBigInteger()
+            }
+
+        }
+    }
+
+    println("Day 02 part 2 solution: "+invId)
+}
+
