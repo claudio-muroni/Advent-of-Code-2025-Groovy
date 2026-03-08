db217dc972c39bac0fd5f96db5e0e72b85ad07be
claudio-muroni <muroni.claudio@gmail.com>
2026-03-08 16:36:30 +0100

    Solved Day 03 part 1

--- src/Day03_1.groovy
+++ src/Day03_1.groovy
@@ -1,4 +1,35 @@
 static void main() {
-    def line = new File("resources/Input_Day03_1.txt").readLines()
+    def batteryBanks = new File("resources/Input_Day03_1.txt").readLines()
+
+    BigInteger totJoltage = 0
+
+    for (batteryBank in batteryBanks) {
+
+        def firstNumPos = -1
+        def secondNumPos = -1
+        def firstNum = ""
+        def secondNum = ""
+
+        for (num in 9..1) {
+            firstNumPos = batteryBank.indexOf(num.toString())
+            if (firstNumPos >= 0 && firstNumPos<batteryBank.length()-1) {
+                firstNum = num.toString()
+                break
+            }
+        }
+
+        for (num in 9..1) {
+            secondNumPos = batteryBank.indexOf(num.toString(), firstNumPos+1)
+            if (secondNumPos > firstNumPos) {
+                secondNum = num.toString()
+                break
+            }
+        }
+
+        def joltage = (firstNum+secondNum).toInteger()
+        totJoltage += joltage
+    }
+
+    println("Day 03 part 1 solution: "+totJoltage)
 }
 
